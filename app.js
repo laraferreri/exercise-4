00e73e96b7ca9e7412a41e0a2d3a7df2f9b59d9c
laraferreri <lbf287@nyu.edu>
2020-11-05 14:43:12 -0500

    second

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = process.env.POST || 4000;
 
+const indexRoute = require("./routes/index.js")
+
+app.use("/", indexRoute);
+
 app.listen(port, () =>
 console.log(`Exercise Four is running at localhost: ${port}`)
 );
