64d1447d609a8357af45c38b349a3a1dbc35c93a
laraferreri <lbf287@nyu.edu>
2020-11-12 12:16:01 -0500

    class

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ const app = express();
 
 const port = process.env.PORT|| 4000;
 
+var firebaseConfig = {
+    apiKey: "AIzaSyBztW-ITDjc9nb2DuPh1E8n-apLJz3YAvM",
+    authDomain: "exercise-four-8dae2.firebaseapp.com",
+    databaseURL: "https://exercise-four-8dae2.firebaseio.com",
+    projectId: "exercise-four-8dae2",
+    storageBucket: "exercise-four-8dae2.appspot.com",
+    messagingSenderId: "793231468965",
+    appId: "1:793231468965:web:9f8f8ce6b05d8f1a28e04c"
+  };
+
+  const firebase = require("firebase");
+  firebase.initializeApp(firebaseConfig);
+
 const indexRoute = require("./routes/index.js")
+const postRoute = require("./routes/post.js")
+const createRoute = require("./routes/createBlogpost.js")
 
 app.use("/", indexRoute);
+app.use("/post", postRoute);
+app.use("/create", createRoute);
 
 app.listen(port, () =>
 console.log(`Exercise Four is running at localhost:${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req,res) => res.send("Exercise 4"));
+const firebase = require ("firebase");
+const db = firebase.firestore();
+const blogposts = db.collection("blogposts");
+
+router.get("/", (req,res) => {
+   blogposts.get().then((querySnapshot) => {
+       console.log("querySnapshot", querySnapshot)
+       return res.send(querySnapshot)
+        })
+        .catch(function(e){
+            console.log("error", e);
+            return res.send(error);
+    });
+});
+    
+// res.send("Show all Blog Posts"));
 
 module.exports = router; 
