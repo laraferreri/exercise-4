fc4287016e0c7ecd795873b4e20181351d43b468
laraferreri <lbf287@nyu.edu>
2020-11-17 14:53:32 -0500

    FINAL

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -7,8 +7,8 @@ const blogposts = db.collection("blogposts");
 const form = `
 <form action="/create/submit">
 <input type= "text" name="title" placeholder="Title of Post" />
-<input type= "text" name="title" placeholder="Text of Post" />
-<input type= "text" name="title" placeholder="Author" />
+<input type= "text" name="text" placeholder="Text of Post" />
+<input type= "text" name="author" placeholder="Author" />
 <button type= "submit"> Submit Post</button>
 
 </form>
@@ -18,7 +18,7 @@ router.get("/", (req, res) => res.send(form));
 
 router.get("/submit", (req, res) => {
   const queryParams = req.query;
-  const idFromTitle = queryParams.title.replace(/\+/g, "-").toLowerCase();
+  const idFromTitle = queryParams.title.replace(/\s+/g, "-").toLowerCase();
   blogposts
     .doc(idFromTitle)
     .set(queryParams)
